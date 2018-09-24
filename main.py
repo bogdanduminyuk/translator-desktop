ca3a7826684647428d332c348e6d37da78906729
bogdanduminyuk <77934329bogdan@gmail.com>
2018-09-24 21:44:19 +0300

    one request to a wooordhunt (translation) is realized in main

--- main.py
+++ main.py
@@ -1,6 +1,37 @@
+import time
+import requests
+from bs4 import BeautifulSoup as bs
+from core import settings
 from core.input_data_readers import OneLineReader
+from core.result_data_writers import TxtDataWriter
+
+input_path = "data/one_line_words.txt"
+output_path = "data/output.txt"
 
 reader = OneLineReader()
-data = reader.read("data/one_line_words.txt")
-print(data)
+writer = TxtDataWriter(output_path)
+data = reader.read(input_path)
+url = settings.urls["translation"]
+output = {}
 
+print("Read data from file: ", input_path)
+
+for word in data:
+    response = requests.get(url.format(word_id=word), headers=settings.headers, timeout=settings.request_timeout)
+
+    if response.ok:
+        soup = bs(response.text, features="html.parser")
+        selection = soup.select(settings.selector)
+
+        if selection:
+            selection = selection[0].text
+
+        print("'{}': '{}'".format(word, selection))
+        output[word] = selection
+        time.sleep(0.33)
+    else:
+        raise ConnectionError(response)
+
+writer.write(output)
+print("OK")
+print("Look for result file: ", output_path)
