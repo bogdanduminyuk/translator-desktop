b7f0daf100bc774c7f078678da81c891b51aaffe
bogdanduminyuk <77934329bogdan@gmail.com>
2018-09-24 21:43:35 +0300

    added simple TxtWriter. It writes in format of word: values line by line

---
+++ core/result_data_writers.py
@@ -0,0 +1,11 @@
+from .abstracts import ResultDataWriter
+
+
+class TxtDataWriter(ResultDataWriter):
+    def __init__(self, path):
+        self.path = path
+
+    def write(self, data):
+        with open(self.path, "w", encoding="utf-8") as file:
+            for key, value in data.items():
+                file.write(key + ": " + value + "\n")
