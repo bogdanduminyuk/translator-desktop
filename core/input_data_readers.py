02357a0bb5889229e0ace24d7a10c344ae2983b8
bogdanduminyuk <77934329bogdan@gmail.com>
2018-09-25 21:16:46 +0300

    OneLineReader renamed TxtFileReader. It reads multiple lines for now and convert them into the list of strings

--- core/input_data_readers.py
+++ core/input_data_readers.py
@@ -3,11 +3,14 @@ import os
 from .abstracts import InputDataReader
 
 
-class OneLineReader(InputDataReader):
+class TxtFileReader(InputDataReader):
     def read(self, path):
         if not os.path.exists(path):
             raise FileNotFoundError(path)
 
         with open(path, "r") as file:
-            data = file.read()
-            return data.split(',')
+            data = file.read().split(",")
+
+            for i in range(len(data)):
+                data[i] = data[i].strip()
+            return data
