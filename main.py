6eeed8c81bcebad3ab699b633546df7179f81433
bogdanduminyuk <77934329bogdan@gmail.com>
2018-09-26 19:09:46 +0300

    add exception handling in main

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import requests
 from core import settings
 from core.input_data_readers import TxtFileReader
 from core.result_data_writers import TxtDataWriter
-from core.functions import is_collocation, get_translation
-
+from core.functions import is_collocation, get_translation, log
 
 input_path = "data/one_line_words.txt"
 output_path = "data/output.txt"
@@ -15,21 +14,25 @@ if __name__ == "__main__":
     output = {}
 
     data = reader.read(input_path)
-    url = settings.urls["translation"]
 
     print("Read data from file: ", input_path)
 
     for word in data:
-        response = requests.get(url.format(word_id=word), headers=settings.headers, timeout=settings.request_timeout)
+        url = settings.urls["translation"].format(word_id=word)
 
-        if response.ok:
-            translation = get_translation(response.text, is_collocation(word))
+        try:
+            response = requests.get(url.format(word_id=word), headers=settings.headers, timeout=settings.request_timeout)
 
-            print("'{}': '{}'".format(word, translation))
-            output[word] = translation
-            time.sleep(0.33)
-        else:
-            raise ConnectionError(response)
+            if response.ok:
+                translation = get_translation(response.text, is_collocation(word))
+
+                print("'{}': '{}'".format(word, translation))
+                output[word] = translation
+                time.sleep(0.33)
+            else:
+                raise ConnectionError(response)
+        except Exception as e:
+            log(word, url, str(e))
 
     writer.write(output)
     print("OK")
