f1090d255e44e715a1412abc2561d2bb5a15d7a5
bogdanduminyuk <77934329bogdan@gmail.com>
2018-10-02 22:33:57 +0300

    method for parsing output scraper data was added

--- try_docx.py
+++ try_docx.py
@@ -1,3 +1,5 @@
+import json
+
 from docx import Document
 from docx.shared import Cm, Pt
 
@@ -20,6 +22,67 @@ def stub_rows():
         ('4', 'word', 'translation', 'synonyms', 'antonyms', 'definitions'),
         ('5', 'word', 'translation', 'synonyms', 'antonyms', 'definitions'),
     )
+
+
+def get_records():
+    def get_data():
+        with open("data/output.txt", "r", encoding="utf-8") as file:
+            return json.loads(file.read())
+
+    def get_translations(data):
+        return {
+            word: value["translations"]["simple"]
+            for word, value in data.items()
+        }
+
+    def parse_api(data):
+        displayed_data = []
+        synonyms, antonyms, definitions = "", "", ""
+
+        for word in data.keys():
+            lexical_entries = data[word]["api"][0]["lexicalEntries"]
+
+            for lexical_category in lexical_entries:
+                synonyms_list = lexical_category.get("synonyms", [])
+                antonyms_list = lexical_category.get("antonyms", [])
+                definitions_list = lexical_category.get("definitions", [])
+
+                if synonyms_list:
+                    synonyms += "Категория: " + lexical_category["lexicalCategory"] + "\n"
+                    for synonym in synonyms_list:
+                        synonyms += synonym + "\n"
+
+                if antonyms_list:
+                    antonyms += "Категория: " + lexical_category["lexicalCategory"] + "\n"
+                    for antonym in antonyms_list:
+                        antonyms += antonym + "\n"
+
+                if definitions_list:
+                    definitions += "Категория: " + lexical_category["lexicalCategory"] + "\n"
+                    for definition in definitions_list:
+                        definitions += definition + "\n"
+
+            displayed_data.append((
+                word, synonyms, antonyms, definitions
+            ))
+
+        return displayed_data
+
+    data = get_data()
+    translations = get_translations(data)
+    parsed_api = parse_api(data)
+
+    rows = []
+
+    i = 1
+
+    for word, synonyms, antonyms, definitions in parsed_api:
+        rows.append(
+            (str(i), word, translations[word], synonyms, antonyms, definitions)
+        )
+        i += 1
+
+    return rows
 
 
 def switch_orientation(doc):
@@ -55,7 +118,8 @@ if __name__ == "__main__":
     set_margins(document, settings["margin"])
     change_font(document, settings["font-family"], settings["font-size"], settings["line-spacing"])
 
-    records = stub_rows()
+    # records = stub_rows()
+    records = get_records()
 
     table = document.add_table(rows=len(records), cols=6)
     table.style = 'Table Grid'
