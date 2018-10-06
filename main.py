b4b71bf0ffb8799d0b8578edd746e49009064feb
bogdanduminyuk <77934329bogdan@gmail.com>
2018-10-06 18:49:46 +0300

    saving|getting with db was added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from core.input_data_readers import TxtFileReader
 from core.result_data_writers import DocDataWriter
 from core.functions import get_translation, get_word_from_api
+from core.db import Database
 
 input_path = "data/input.txt"
 output_path = "data/output.docx"
@@ -19,10 +20,17 @@ if __name__ == "__main__":
     print("Read data from file: ", input_path)
 
     for word in data:
-        output[word] = {
-            "api": get_word_from_api(word),
-            "translations": get_translation(word),
-        }
+        from_db = Database.get(word)
+
+        if from_db:
+            output[word] = from_db
+        else:
+            output[word] = {
+                "api": get_word_from_api(word),
+                "translation": get_translation(word),
+            }
+
+            Database.set(word, output[word]["translation"], output[word]["api"][0])
 
         print(word + ".. OK")
 
