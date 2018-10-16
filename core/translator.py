cc75ce96fad9305a56aa46dfac7f1da838dfd26f
bogdanduminyuk <77934329bogdan@gmail.com>
2018-10-16 20:58:25 +0300

    stub was improved for translator.get(word)

--- core/translator.py
+++ core/translator.py
@@ -3,4 +3,9 @@ import time
 
 def get(word):
     time.sleep(1)
-    return word
+    return {
+        "word": word,
+        "syn": word + " synonyms",
+        "ant": word + " antonyms",
+        "def": word + " definitions",
+    }
