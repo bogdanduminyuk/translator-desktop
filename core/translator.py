d14cd099c90bde439234b59a2645fc8e19421e7f
bogdanduminyuk <77934329bogdan@gmail.com>
2018-10-21 22:39:08 +0300

    add forgotten translation stub and minimize time of sleep

--- core/translator.py
+++ core/translator.py
@@ -2,10 +2,11 @@ import time
 
 
 def get(word):
-    time.sleep(1)
+    time.sleep(0.2)
     return {
         "word": word,
         "syn": word + " synonyms",
         "ant": word + " antonyms",
         "def": word + " definitions",
+        "translate": word + " translation"
     }
